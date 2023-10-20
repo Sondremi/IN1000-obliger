b5f065afd1975b5fe389df677ac745bbe176eec5
Sondre Indset <sondre2004@hotmail.no>
2023-10-20 17:12:32 +0200

    Update funksjoner.py

--- Oblig5/funksjoner.py
+++ Oblig5/funksjoner.py
@@ -5,12 +5,13 @@ sjekker antall forekomster av en bokstav i en tekst
 # 1.1
 # Oppretter funksjonen adder med to parametere
 def adder(tall1, tall2):
-    # Returnerer en tekst hvor bruker får se begge tallene og summen
-    return(f"Summen av {tall1} + {tall2} = {tall1 + tall2}")
+    # Returnerer tallene addert
+    return tall1 + tall2
 
 # Kaller på funksjonen med de to parameterene 
-print(adder(3, 7))
-print(adder(12, 38))
+print(f"Summen av 3+7 = {adder(3, 7)}")
+print(f"Summen av 12+8 = {adder(12, 8)}")
+
 
 # 1.2 / 1.3
 # Oppretter funksjonen med to parametere
@@ -25,13 +26,12 @@ def tellForekoster(minTekst, minBokstav):
         if minBokstav == bokstav:
             antall_forekomster += 1
 
-    # Har en if-test her for å gi bruker en bedre print
-    # flere ganger, istedet for 1 gang
-    if antall_forekomster > 1 or antall_forekomster == 0:
-        return(f"Bokstaven {minBokstav} forekom {antall_forekomster} ganger i teksten")
-    
-    return(f"Bokstaven {minBokstav} forekom {antall_forekomster} gang i teksten")
+    # Returnerer verdien
+    return antall_forekomster
 
-# Kaller på funksjonen
-# Her er parameterne to input-felt som bruker oppgir når funksjonen blir kalt 
-print(tellForekoster(input("Skriv inn en tekst: "), input("Oppgi en bokstav: ")))
+# Ber bruker om input på tekst og bokstav
+input_tekst = input("Skriv inn en tekst: ")
+input_bokstav = input("Skriv inn et bokstav: ")
+
+# Kaller på funksjonen med bruker-input som argument
+print(tellForekoster(input_tekst, input_bokstav))
